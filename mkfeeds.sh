9ea0739494ff8758dffd0092c821693be6d15859
pipfrosch <pipfroschpress@gmail.com>
2020-06-25 23:59:56 -0700

    mammals group feed

--- mkfeeds.sh
+++ mkfeeds.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for json in JoM.json; do
+for json in JoM.json mammals.json; do
   noitalics="`echo ${json} |sed -e s?"\.json$"?"-noitalics.json"?`"
   cat ${json} |sed -e s?"\.atom"?"-noitalics.atom"?g > ${noitalics}
   touch -r ${json} ${noitalics}
