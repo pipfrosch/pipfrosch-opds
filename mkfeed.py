ff86e354c9affe37414b4e76bf63329832557245
pipfrosch <pipfroschpress@gmail.com>
2020-06-26 19:20:00 -0700

    finished (for now)

--- mkfeed.py
+++ mkfeed.py
@@ -4,7 +4,6 @@ import os
 import pathlib
 import time
 import datetime
-import pytz
 import json
 import re
 from xml.dom import minidom
@@ -15,34 +14,35 @@ time.tzset()
 
 # https://specs.opds.io/opds-1.2.html
 
-def validateUUID(string):
+# Pipfrosch Press only uses Version 4 UUID
+def validateUUID(string, jsonfile):
     if type(string) != str:
-        print(string + " is not a valid UUID urn string.")
+        print('Error in ' + jsonfile + ': ' + string + ' is not a valid UUID urn string.')
         sys.exit(1)
     if len(string) != 45:
-        print(string + " is not a valid UUID urn string.")
+        print('Error in ' + jsonfile + ': ' + string + ' is not a valid UUID urn string.')
         sys.exit(1)
     header = string[0:9]
-    if header != "urn:uuid:":
-        print(string + " is not a valid UUID urn string.")
+    if header != 'urn:uuid:':
+        print('Error in ' + jsonfile + ': ' + string + ' is not a valid UUID urn string.')
         sys.exit(1)
     uuidstring = string[9:]
     pattern = re.compile(r'^[\da-f]{8}-([\da-f]{4}-){3}[\da-f]{12}$', re.IGNORECASE)
     match = re.search(pattern, uuidstring)
     if not match:
-        print(string + " is not a valid UUID urn string.")
+        print('Error in ' + jsonfile + ': ' + string + ' is not a valid UUID urn string.')
         sys.exit(1)
     uuidlist = list(uuidstring)
-    if uuidlist[14] != "4":
-        print(uuidstring + " is not a valid Version 4 UUID.")
+    if uuidlist[14] != '4':
+        print('Error in ' + jsonfile + ': ' + uuidstring + ' is not a valid Version 4 UUID.')
         sys.exit(1)
-    if uuidlist[19] != "8":
-        print("first character of fourth block in " + uuidstring + " is not 8")
+    if uuidlist[19] != '8':
+        print('Error in ' + jsonfile + ': ' + 'first character of fourth block in ' + uuidstring + ' is not 8')
         sys.exit(1)
 
 def validateNamespaces(dictionary, jsonfile):
     if type(dictionary) != dict:
-        print("The namespaces key in " + jsonfile + " does not point to a valid dictionary.")
+        print('The namespaces key in ' + jsonfile + ' does not point to a valid dictionary.')
         sys.exit(1)
     keylist = dictionary.keys()
     pattern = re.compile(r'^[a-z]+$')
@@ -74,6 +74,41 @@ def validateNamespaces(dictionary, jsonfile):
             print('Error in ' + jsonfile + ': The value associated with the namespaces key ' + ns + ' is not a valid uri.')
             sys.exit(1)
 
+def validateLinks(links, jsonfile):
+    if type(links) != list:
+        print('Error in ' + jsonfile + ': The links key in " + jsonfile + " does not point to a valid list.')
+        sys.exit(1)
+    reqatt = ['rel', 'href', 'type']
+    for link in links:
+        if type(link) != dict:
+            print('Error in ' + jsonfile + ': Not all entries in links list are key=value dictionaries.')
+            sys.exit(1)
+        keys = link.keys()
+        for att in reqatt:
+            if att not in keys:
+                print('Error in ' + jsonfile + ': Some dictionaries in links list are missing the ' + att + ' key.')
+                sys.exit(1)
+            if type(link.get(att)) != str:
+                print('Error in ' + jsonfile + ': The links value associated with ' + att + ' is not a string.')
+                sys.exit(1)
+
+def validateAuthors(authors, jsonfile):
+    if type(authors) != list:
+        print('Error in ' + jsonfile + ': The authors key in ' + jsonfile + ' does not point to a valid list.')
+        sys.exit(1)
+    for author in authors:
+        if type(author) != dict:
+            print('Error in ' + jsonfile + ': Some author entries in ' + jsonfile + ' are not dictionaries.')
+            sys.exit(1)
+        keys = author.keys()
+        if 'name' not in author:
+            print('Error in ' + jsonfile + ': Some author entries in ' + jsonfile + ' do not have a name key.')
+            sys.exit(1)
+        for key in keys:
+            if type(author.get(key)) != str:
+                print('Error in ' + jsonfile + ': Some author keys in ' + jsonfile + ' do not have string values.')
+                sys.exit(1)
+
 def createAtomFeed(cwd, jsonfile):
     mtime = []
     try:
@@ -81,89 +116,66 @@ def createAtomFeed(cwd, jsonfile):
     except:
         print('File ' + jsonfile + ' does not exist.')
         sys.exit(1)
-    mtime.append(ts.strftime("%Y-%m-%dT%H:%M:%SZ"))
+    mtime.append(ts.strftime('%Y-%m-%dT%H:%M:%SZ'))
     try:
         with open(jsonfile) as f:
             jsondata = json.load(f)
     except:
         print(jsonfile + ' does not appear to be valid JSON.')
         sys.exit(1)
-    if "output" not in jsondata.keys():
+    if 'output' not in jsondata.keys():
         print(jsonfile + ' does not specify proper output file.')
         sys.exit(1)
-    if type(jsondata.get("output")) != str:
-        print("Value for output key in " + jsonfile + " is not a string.")
+    if type(jsondata.get('output')) != str:
+        print('Value for output key in ' + jsonfile + ' is not a string.')
         sys.exit(1)
-    string = jsondata.get("output")
+    string = jsondata.get('output')
     atom = os.path.join(cwd, string)
-    string = "<feed/>"
+    string = '<feed/>'
     mydom = minidom.parseString(string)
     root = mydom.getElementsByTagName('feed')[0]
     # add namespaces
     root.setAttribute('xmlns', 'http://www.w3.org/2005/Atom')
-    if "namespaces" in jsondata.keys():
-        namespaces = jsondata.get("namespaces")
+    if 'namespaces' in jsondata.keys():
+        namespaces = jsondata.get('namespaces')
         validateNamespaces(namespaces, jsonfile)
         nskeys = namespaces.keys()
         for ns in nskeys:
             root.setAttribute('xmlns:' + ns, namespaces.get(ns))
     # get id
-    if "id" not in jsondata.keys():
+    if 'id' not in jsondata.keys():
         print(jsonfile + ' does not specify id.')
         sys.exit(1)
-    validateUUID(jsondata.get("id"))
-    stringlist = list(jsondata.get("id"))
+    validateUUID(jsondata.get('id'), jsonfile)
+    stringlist = list(jsondata.get('id'))
     if "-noitalics" in jsonfile:
         # indicate noitalics by changing first hex of fourth group to 9
-        stringlist[28] = "9"
+        stringlist[28] = '9'
     string = ''.join(stringlist)
     text = mydom.createTextNode(string)
     node = mydom.createElement('id')
     node.appendChild(text)
     root.appendChild(node)
     # get links
-    if "links" not in jsondata.keys():
+    if 'links' not in jsondata.keys():
         print(jsonfile + ' does not specify links.')
         sys.exit(1)
     links = jsondata.get("links")
-    if type(links) != list:
-        print("The links key in " + jsonfile + "is not a list.")
-        sys.exit(1)
+    validateLinks(links, jsonfile)
     for link in links:
-        if type(link) != dict:
-            print("some entries in the links key in " + jsonfile  + " are not a key=value dictionary.")
-            sys.exit(1)
         node = mydom.createElement('link')
-        if "rel" not in link.keys():
-            print("some dictionaries in the links key in " + jsonfile + " do not have a rel key.")
-            sys.exit(1)
-        if type(link.get("rel")) != str:
-            print("Some rel keys in dictionaries within the links key in " + jsonfile + " do not have a string value.")
-            sys.exit(1)
-        node.setAttribute("rel", link.get("rel"))
-        if "href" not in link.keys():
-            print("some dictionaries in the links key in " + jsonfile + " do not have a href key.")
-            sys.exit(1)
-        if type(link.get("href")) != str:
-            print("Some href keys in dictionaries within the links key in " + jsonfile + " do not have a string value.")
-            sys.exit(1)
-        node.setAttribute("href", link.get("href"))
-        if "type" not in link.keys():
-            print("some dictionaries in the links key in " + jsonfile + " do not have a type key.")
-            sys.exit(1)
-        if type(link.get("type")) != str:
-            print("Some type keys in dictionaries within the links key in " + jsonfile + " do not have a string value.")
-            sys.exit(1)
-        node.setAttribute("type", link.get("type"))
+        node.setAttribute('rel', link.get('rel'))
+        node.setAttribute('href', link.get('href'))
+        node.setAttribute('type', link.get('type'))
         root.appendChild(node)
     # feed title
-    if "title" not in jsondata.keys():
+    if 'title' not in jsondata.keys():
         print(jsonfile + ' does not specify title.')
         sys.exit(1)
-    if type(jsondata.get("title")) != str:
-        print("The title value in " + jsonfile + " is not a string.")
+    if type(jsondata.get('title')) != str:
+        print('The title value in ' + jsonfile + ' is not a string.')
         sys.exit(1)
-    string = jsondata.get("title")
+    string = jsondata.get('title')
     text = mydom.createTextNode(string)
     node = mydom.createElement('title')
     node.appendChild(text)
@@ -172,46 +184,70 @@ def createAtomFeed(cwd, jsonfile):
     modified = mydom.createElement('updated')
     root.appendChild(modified)
     # author(s)
-    if "authors" not in jsondata.keys():
+    if 'authors' not in jsondata.keys():
         print(jsonfile + ' does not specify author(s).')
         sys.exit(1)
-    authors = jsondata.get("authors")
+    authors = jsondata.get('authors')
+    validateAuthors(authors, jsonfile)
     for author in authors:
-        string = author.get("name")
-        text = mydom.createTextNode(string)
-        name = mydom.createElement("name")
-        name.appendChild(text)
-        string = author.get("uri")
-        text = mydom.createTextNode(string)
-        uri = mydom.createElement("uri")
-        uri.appendChild(text)
         authornode = mydom.createElement('author')
+        string = author.get('name')
+        text = mydom.createTextNode(string)
+        name = mydom.createElement('name')
+        name.appendChild(text)
         authornode.appendChild(name)
-        authornode.appendChild(uri)
+        if 'uri' in author.keys():
+            string = author.get('uri')
+            text = mydom.createTextNode(string)
+            uri = mydom.createElement('uri')
+            uri.appendChild(text)
+            authornode.appendChild(uri)
         root.appendChild(authornode)
     testcounter = 0
     # acquisition nodes
-    if "acquisitions" in jsondata.keys():
+    if 'acquisitions' in jsondata.keys():
         testcounter += 1
-        acquisitions = jsondata.get("acquisitions")
+        acquisitions = jsondata.get('acquisitions')
+        if type(acquisitions) != list:
+            print('The acquisitions entry in ' + jsonfile + ' is not a list.')
+            sys.exit(1)
         for feed in acquisitions:
             feedjson = os.path.join(cwd, feed)
-            with open(feedjson) as g:
-                feeddata = json.load(g)
-            entry = mydom.createElement("entry")
+            try:
+                with open(feedjson) as g:
+                    feeddata = json.load(g)
+            except:
+                print(feedjson + ' does not appear to be valid JSON.')
+                sys.exit(1)
+            entry = mydom.createElement('entry')
             # feed title
-            string = feeddata.get("title")
+            if 'title' not in feeddata.keys():
+                print(feedjson + ' does not specify title.')
+                sys.exit(1)
+            if type(feeddata.get('title')) != str:
+                print('The title value in ' + feedjson + ' is not a string.')
+                sys.exit(1)
+            string = feeddata.get('title')
             text = mydom.createTextNode(string)
             node = mydom.createElement('title')
             node.appendChild(text)
             entry.appendChild(node)
             # feed link
             node = mydom.createElement('link')
-            if "rootrel" in feeddata.keys():
+            if 'rootrel' in feeddata.keys():
+                if type(feeddata.get('rootrel')) != str:
+                    print('The rootrel entry in ' + feedjson + ' is not a string.')
+                    sys.exit(1)
                 string = feeddata.get('rootrel')
             else:
                 string = 'subsection'
             node.setAttribute('rel', string)
+            if 'output' not in feeddata.keys():
+                print(feedjson + ' does not specify its output file.')
+                sys.exit(1)
+            if type(feeddata.get('output')) != str:
+                print('Value for output key in ' + feedjson + ' is not a string.')
+                sys.exit(1)
             prestring = feeddata.get('output')
             string = prestring[4:]
             node.setAttribute('href', string)
@@ -219,51 +255,82 @@ def createAtomFeed(cwd, jsonfile):
             entry.appendChild(node)
             # updated
             atomfile = os.path.join(cwd, feeddata.get('output'))
-            atomdom = minidom.parse(atomfile)
-            modifiednode = atomdom.getElementsByTagName('updated')[0]
-            nodevalue = modifiednode.firstChild.nodeValue
-            text = mydom.createTextNode(nodevalue)
-            node = mydom.createElement('updated')
-            node.appendChild(text)
-            entry.appendChild(node)
-            # get the ID
-            idnode = atomdom.getElementsByTagName('id')[0]
-            nodevalue = idnode.firstChild.nodeValue
-            text = mydom.createTextNode(nodevalue)
-            node = mydom.createElement('id')
-            node.appendChild(text)
-            entry.appendChild(node)
+            try:
+                atomdom = minidom.parse(atomfile)
+            except:
+                print('Could not parse the XML Atom file ' + atomfile)
+                sys.exit(1)
+            # import updated node
+            nodelist = atomdom.getElementsByTagName('updated')
+            if len(nodelist) == 0:
+                print('The XML Atom file ' + atomfile + 'does not contain an updated node.')
+                sys.exit(1)
+            impnode = mydom.importNode(nodelist[0], True)
+            entry.appendChild(impnode)
+            # import id node
+            nodelist = atomdom.getElementsByTagName('id')
+            if len(nodelist) == 0:
+                print('The XML Atom file ' + atomfile + 'does not contain an id node.')
+                sys.exit(1)
+            impnode = mydom.importNode(nodelist[0], True)
+            entry.appendChild(impnode)
             # content
-            string = feeddata.get("content")
-            text = mydom.createTextNode(string)
-            node = mydom.createElement('content')
-            node.setAttribute('type', 'text')
-            node.appendChild(text)
-            entry.appendChild(node)
+            if 'content' in feeddata.keys():
+                string = feeddata.get('content')
+                if type(string) != str:
+                    print('The content value in ' + feedjson + ' is not a string.')
+                    sys.exit(1)
+                text = mydom.createTextNode(string)
+                node = mydom.createElement('content')
+                node.setAttribute('type', 'text')
+                node.appendChild(text)
+                entry.appendChild(node)
             # append the entry
             root.appendChild(entry)
     # entry nodes
-    if "entries" in jsondata.keys():
+    if 'entries' in jsondata.keys():
         testcounter += 1
-        entries = jsondata.get("entries")
-        for atomfile in entries:
-            filepath = os.path.join(cwd, atomfile)
-            ts = datetime.datetime.fromtimestamp(os.path.getmtime(filepath))
-            mtime.append(ts.strftime("%Y-%m-%dT%H:%M:%SZ"))
-            entrydom = minidom.parse(filepath)
-            entrynode = mydom.importNode(entrydom.childNodes[0], True)
-            entrynode.removeAttribute('xmlns')
-            entrynode.removeAttribute('xmlns:dc')
-            whitelist = ["text/html", "image/jpeg", "image/png", "application/epub+zip"]
-            linklist = entrynode.getElementsByTagName("link")
+        entries = jsondata.get('entries')
+        if type(entries) != list:
+            print('The entries key in ' + jsonfile + ' is not for a list.')
+            sys.exit(1)
+        for filename in entries:
+            if type(filename) != str:
+                print('The entries list in ' + jsonfile + ' contains types other than strings.')
+                sys.exit(1)
+            atomfile = os.path.join(cwd, filename)
+            ts = datetime.datetime.fromtimestamp(os.path.getmtime(atomfile))
+            mtime.append(ts.strftime('%Y-%m-%dT%H:%M:%SZ'))
+            try:
+                atomdom = minidom.parse(atomfile)
+            except:
+                print('Could not parse the XML Atom file ' + atomfile)
+                sys.exit(1)
+            nodelist = atomdom.getElementsByTagName('entry')
+            if len(nodelist) == 0:
+                print('The XML Atom file ' + atomfile + 'does not contain an entry node.')
+                sys.exit(1)
+            impnode = mydom.importNode(nodelist[0], True)
+            attributes = impnode.attributes.items()
+            for att in attributes:
+                attname = att[0]
+                if len(attname) > 4:
+                    test = attname[0:5]
+                    if test == 'xmlns':
+                        impnode.removeAttribute(attname)
+            whitelist = ['text/html', 'image/jpeg', 'image/png', 'application/epub+zip']
+            linklist = impnode.getElementsByTagName('link')
             i = len(linklist) - 1
             while i >= 0 :
                 node = linklist[i]
-                ntype = node.getAttribute('type')
-                if ntype not in whitelist:
-                    entrynode.removeChild(node)
+                if node.hasAttribute('type'):
+                    ntype = node.getAttribute('type')
+                    if ntype not in whitelist:
+                        impnode.removeChild(node)
+                else:
+                    impnode.removeChild(node)
                 i -= 1
-            root.appendChild(entrynode)
+            root.appendChild(impnode)
     if testcounter == 0:
         print(jsonfile + ' does not have acquisition or entry nodes.')
         sys.exit(1)
@@ -275,7 +342,7 @@ def createAtomFeed(cwd, jsonfile):
     string = mydom.toprettyxml(indent="  ",newl="\n",encoding="UTF-8").decode()
     string = '\n'.join([x for x in string.split("\n") if x.strip()!=''])
     try:
-        fh = open(atom, "w")
+        fh = open(atom, 'w')
         fh.write(string)
         fh.close()
     except:
@@ -284,10 +351,10 @@ def createAtomFeed(cwd, jsonfile):
 
 def main():
     if len(sys.argv) != 2:
-        print("fubar")
+        print('fubar')
         sys.exit(1)
     cwd = os.getcwd()
     createAtomFeed(cwd, sys.argv[1])
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
